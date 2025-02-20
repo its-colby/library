5bb0b1962a61a5d82efd2ec4bf9a7d100285491b
Colby <colby@Colbys-Laptop.local>
2025-02-19 17:20:03 -0800

    import bug

--- src/routes/mathematics/topology/+page.svelte
+++ src/routes/mathematics/topology/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import * as x from "$lib/articles/mathematics/topology";
-    import Container from "$lib/ui/article/Container.svelte";
-    import SectionContainer from "$lib/ui/article/section/SectionContainer.svelte";
-    import TexContent from "$lib/ui/article/section/Content.svelte";
-    import Title from "$lib/ui/article/Title.svelte";
-    import SectionTitle from "$lib/ui/article/section/SectionTitle.svelte";
+    // import * as x from "$lib/articles/mathematics/topology";
+    // import Container from "$lib/ui/article/Container.svelte";
+    // import SectionContainer from "$lib/ui/article/section/SectionContainer.svelte";
+    // import TexContent from "$lib/ui/article/section/Content.svelte";
+    // import Title from "$lib/ui/article/Title.svelte";
+    // import SectionTitle from "$lib/ui/article/section/SectionTitle.svelte";
 </script>
 
 <!-- <Container>
