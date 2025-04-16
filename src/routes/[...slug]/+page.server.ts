1b384087da69f66a774a10545675315b8466a911
Colby <colby@Colbys-Laptop.local>
2025-04-16 15:35:18 -0700

    server bug fix?

--- src/routes/[...slug]/+page.server.ts
+++ src/routes/[...slug]/+page.server.ts
@@ -1,7 +1,8 @@
 import { root } from "$content";
 
 export function entries() {
-    const routes = root.published_urls().map(url => ({ slug: url }));
+    const routes = root.published_urls()
+        .map(url => ({ slug: url.replace(/^\//, '') }));
     routes.push({ slug: '404' });
     return routes;
 } 
